6d8c2940e42c9dcea0a07282089426bbf34d38a3
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-03-20 09:12:56 +0100

    Escribiendo implementación crear en el cliente.service.ts y...

--- angular/clientes-app/src/app/clientes/form.component.ts
+++ angular/clientes-app/src/app/clientes/form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import { Router } from '@angular/router';
+import { ClienteService } from './cliente.service';
 
 @Component({
   selector: 'app-form',
@@ -9,13 +11,14 @@ export class FormComponent implements OnInit {
   public cliente: Cliente = new Cliente();
   public titulo: String = "Crear cliente";
 
-  constructor() { }
+  constructor(private clienteService: ClienteService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   public create(): void {
-    console.log("Clicked");
-    console.log(this.cliente);
+    this.clienteService.create(this.cliente).subscribe(
+      response => this.router.navigate(['/clientes'])
+    );
   }
 }
