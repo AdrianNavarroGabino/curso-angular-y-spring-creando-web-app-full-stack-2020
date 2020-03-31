6b2638c013795cd4047fbd9454bd63633b88db88
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-03-31 10:17:59 +0200

    Implementando la paginación en API REST y Repository

--- spring/spring-boot-backend-apirest/src/main/java/com/adriannavarrogabino/models/services/IClienteService.java
+++ spring/spring-boot-backend-apirest/src/main/java/com/adriannavarrogabino/models/services/IClienteService.java
@@ -2,11 +2,16 @@ package com.adriannavarrogabino.models.services;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.adriannavarrogabino.models.entity.Cliente;
 
 public interface IClienteService {
 	
 	public List<Cliente> findAll();
+	
+	public Page<Cliente> findAll(Pageable pageable);
 	
 	public Cliente findById(Long id);
 	
