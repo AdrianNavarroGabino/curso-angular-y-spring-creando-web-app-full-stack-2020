bcb93c4ed86861752f0cbc2c10d54cbecd75c939
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-04-02 11:22:48 +0200

    Afinando más el evento ngOnChanges para refrescar los rangos

--- angular/clientes-app/src/app/paginator/paginator.component.ts
+++ angular/clientes-app/src/app/paginator/paginator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'paginator-nav',
@@ -15,11 +15,19 @@ export class PaginatorComponent implements OnInit, OnChanges {
   constructor() { }
 
   ngOnInit(): void {
-
-
+    this.initPaginator();
   }
 
-  ngOnChanges(changes: import("@angular/core").SimpleChanges): void {
+  ngOnChanges(changes: SimpleChanges): void {
+    let paginadorActualizado = changes['paginador'];
+
+    if(paginadorActualizado.previousValue)
+    {
+      this.initPaginator();
+    }
+  }
+
+  private initPaginator(): void {
     this.desde = Math.min(Math.max(1, this.paginador.number - 4), this.paginador.totalPages - 5);
     this.hasta = Math.max(Math.min(this.paginador.totalPages, this.paginador.number + 4), 6);
 
@@ -32,5 +40,4 @@ export class PaginatorComponent implements OnInit, OnChanges {
       this.paginas = new Array(this.paginador.totalPages).fill(0).map((_valor, indice) => indice + 1);
     }
   }
-
 }
