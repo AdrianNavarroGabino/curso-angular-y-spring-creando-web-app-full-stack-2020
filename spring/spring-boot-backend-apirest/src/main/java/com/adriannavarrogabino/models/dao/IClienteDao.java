a09f8256059b91da29ff3d378e44c28962ca8eed
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-04-06 08:43:45 +0200

    Creando nueva clase entity Region

--- spring/spring-boot-backend-apirest/src/main/java/com/adriannavarrogabino/models/dao/IClienteDao.java
+++ spring/spring-boot-backend-apirest/src/main/java/com/adriannavarrogabino/models/dao/IClienteDao.java
@@ -1,9 +1,15 @@
 package com.adriannavarrogabino.models.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.adriannavarrogabino.models.entity.Cliente;
+import com.adriannavarrogabino.models.entity.Region;
 
 public interface IClienteDao extends JpaRepository<Cliente, Long> {
 
+	@Query("from Region")
+	public List<Region> findAllRegiones();
 }
