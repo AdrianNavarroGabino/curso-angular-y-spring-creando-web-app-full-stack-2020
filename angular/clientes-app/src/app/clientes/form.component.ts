15aa27ae3479423a9f0e35a3dbc201bf33d26d59
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-04-06 09:23:10 +0200

    Añadiendo opción por defecto con el texto seleccionar

--- angular/clientes-app/src/app/clientes/form.component.ts
+++ angular/clientes-app/src/app/clientes/form.component.ts
@@ -66,6 +66,10 @@ export class FormComponent implements OnInit {
   }
 
   compararRegion(o1: Region, o2: Region): boolean {
+    if(o1 === undefined && o2 === undefined)
+    {
+      return true;
+    }
     return o1 == null || o2 == null ? false : o1.id === o2.id;
   }
 }
