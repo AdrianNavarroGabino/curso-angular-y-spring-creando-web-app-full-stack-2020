00b8a3263812ac8107099eed3007889bbf93a428
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-04-07 11:02:36 +0200

    Complementando con más información adicional del usuario autenticado

--- spring/spring-boot-backend-apirest/src/main/java/com/adriannavarrogabino/models/entity/Usuario.java
+++ spring/spring-boot-backend-apirest/src/main/java/com/adriannavarrogabino/models/entity/Usuario.java
@@ -32,10 +32,15 @@ public class Usuario implements Serializable {
 
 	private boolean enabled;
 
+	private String nombre;
+	private String apellido;
+	
+	@Column(unique = true)
+	private String email;
+
 	@ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-	@JoinTable(name = "usuarios_roles", joinColumns = @JoinColumn(name = "usuario_id"),
-		inverseJoinColumns = @JoinColumn(name = "rol_id"),
-		uniqueConstraints = {@UniqueConstraint(columnNames = {"usuario_id", "rol_id"})})
+	@JoinTable(name = "usuarios_roles", joinColumns = @JoinColumn(name = "usuario_id"), inverseJoinColumns = @JoinColumn(name = "rol_id"), uniqueConstraints = {
+			@UniqueConstraint(columnNames = { "usuario_id", "rol_id" }) })
 	private List<Rol> roles;
 
 	public Long getId() {
@@ -76,6 +81,30 @@ public class Usuario implements Serializable {
 
 	public void setRoles(List<Rol> roles) {
 		this.roles = roles;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	private static final long serialVersionUID = 1L;
