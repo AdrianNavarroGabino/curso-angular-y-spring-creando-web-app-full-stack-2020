489b221ae91e31a9ec9598a7e7db4b0f3bad49a6
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-04-09 09:49:44 +0200

    Añadiendo CrudRepository para Factura e implem. métodos en el Service

--- spring/spring-boot-backend-apirest/src/main/java/com/adriannavarrogabino/models/services/ClienteServiceImpl.java
+++ spring/spring-boot-backend-apirest/src/main/java/com/adriannavarrogabino/models/services/ClienteServiceImpl.java
@@ -9,7 +9,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.adriannavarrogabino.models.dao.IClienteDao;
+import com.adriannavarrogabino.models.dao.IFacturaDao;
 import com.adriannavarrogabino.models.entity.Cliente;
+import com.adriannavarrogabino.models.entity.Factura;
 import com.adriannavarrogabino.models.entity.Region;
 
 @Service
@@ -17,6 +19,9 @@ public class ClienteServiceImpl implements IClienteService {
 
 	@Autowired
 	private IClienteDao clienteDao;
+	
+	@Autowired
+	private IFacturaDao facturaDao;
 	
 	@Override
 	@Transactional(readOnly = true)
@@ -52,6 +57,24 @@ public class ClienteServiceImpl implements IClienteService {
 	@Transactional(readOnly = true)
 	public List<Region> findAllRegiones() {
 		return clienteDao.findAllRegiones();
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Factura findFacturaById(Long id) {
+		return facturaDao.findById(id).orElse(null);
+	}
+
+	@Override
+	@Transactional
+	public Factura saveFactura(Factura factura) {
+		return facturaDao.save(factura);
+	}
+
+	@Override
+	@Transactional
+	public void deleteFacturaById(Long id) {
+		facturaDao.deleteById(id);
 	}
 
 }
