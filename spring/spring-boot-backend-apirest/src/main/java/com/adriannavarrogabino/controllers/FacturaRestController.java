a2cec09b28e3cdffee3fc19d77a502a369cdbd19
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-04-09 09:58:52 +0200

    Creando controlador FacturaRestController con la acción handler show

---
+++ spring/spring-boot-backend-apirest/src/main/java/com/adriannavarrogabino/controllers/FacturaRestController.java
@@ -0,0 +1,29 @@
+package com.adriannavarrogabino.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.adriannavarrogabino.models.entity.Factura;
+import com.adriannavarrogabino.models.services.IClienteService;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = {"http://localhost:4200"})
+@RestController
+@RequestMapping("/api")
+public class FacturaRestController {
+	
+	@Autowired
+	private IClienteService clienteService;
+	
+	@GetMapping("/facturas/{id}")
+	@ResponseStatus(code = HttpStatus.OK)
+	public Factura show(@PathVariable Long id) {
+		return clienteService.findFacturaById(id);
+	}
+
+}
