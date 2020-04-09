1cfe5da0677e097bedc4f172d5d82a4681839bd5
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-04-09 08:37:19 +0200

    Asociaciones: ManyToOne Bidireccional - Clases Entity Factura y Cliente

--- spring/spring-boot-backend-apirest/src/main/java/com/adriannavarrogabino/models/entity/Cliente.java
+++ spring/spring-boot-backend-apirest/src/main/java/com/adriannavarrogabino/models/entity/Cliente.java
@@ -1,8 +1,11 @@
 package com.adriannavarrogabino.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -11,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -52,8 +56,15 @@ public class Cliente implements Serializable {
 	@NotNull(message = "La región no puede ser vacía")
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "region_id")
-	@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+	@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
 	private Region region;
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "cliente", cascade = CascadeType.ALL)
+	private List<Factura> facturas;
+
+	public Cliente() {
+		this.facturas = new ArrayList<Factura>();
+	}
 
 	/*
 	 * @PrePersist public void prePersist() { createAt = new Date(); }
@@ -113,6 +124,14 @@ public class Cliente implements Serializable {
 
 	public void setRegion(Region region) {
 		this.region = region;
+	}
+
+	public List<Factura> getFacturas() {
+		return facturas;
+	}
+
+	public void setFacturas(List<Factura> facturas) {
+		this.facturas = facturas;
 	}
 
 	/**
