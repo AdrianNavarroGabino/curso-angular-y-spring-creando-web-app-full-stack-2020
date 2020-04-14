329d46e1aea3d2c0b5b143d0df9fa9737dabd438
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-04-14 09:22:35 +0200

    Reglas de Spring Security

--- spring/spring-boot-backend-apirest/src/main/java/com/adriannavarrogabino/controllers/FacturaRestController.java
+++ spring/spring-boot-backend-apirest/src/main/java/com/adriannavarrogabino/controllers/FacturaRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,12 +27,14 @@ public class FacturaRestController {
 	@Autowired
 	private IClienteService clienteService;
 	
+	@Secured({"ROLE_USER", "ROLE_ADMIN"})
 	@GetMapping("/facturas/{id}")
 	@ResponseStatus(code = HttpStatus.OK)
 	public Factura show(@PathVariable Long id) {
 		return clienteService.findFacturaById(id);
 	}
 	
+	@Secured({"ROLE_ADMIN"})
 	@DeleteMapping("/facturas/{id}")
 	@ResponseStatus(code = HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable Long id)
@@ -39,6 +42,7 @@ public class FacturaRestController {
 		clienteService.deleteFacturaById(id);
 	}
 
+	@Secured({"ROLE_ADMIN"})
 	@GetMapping("/facturas/filtrar-productos/{term}")
 	@ResponseStatus(code = HttpStatus.OK)
 	public List<Producto> filtrarProductos(@PathVariable String term)
@@ -46,6 +50,7 @@ public class FacturaRestController {
 		return clienteService.findProductoByNombre(term);
 	}
 	
+	@Secured({"ROLE_ADMIN"})
 	@PostMapping("/facturas")
 	@ResponseStatus(code = HttpStatus.CREATED)
 	public Factura crear(@RequestBody Factura factura)
